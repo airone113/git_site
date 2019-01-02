5d6099b5132a201d216cfb45d7d3c07b5fa612f4
airone113 <baron.erwann13@gmail.com>
2019-01-02 11:20:38 +0100

    version 1 de mon index

---
+++ index.php
@@ -0,0 +1,14 @@
+Installation de git
+
+Créer un compte sur GitHub
+
+Création projet en local => Ouvrir avec GIT BASH
+
+	- $ git init : initialiser votre projet => on obtient un master
+	
+	- $ git config : --global user.name
+	
+	-
+	
+	
+	
